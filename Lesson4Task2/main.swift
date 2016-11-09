b2a14b4f51e24c0291402ddb838acb99abfa3c81
Kirill Netavskiy <simu4.net@gmail.com>
2016-11-09 19:41:14 +0200

    Initial Commit

---
+++ Lesson4Task2/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Lesson4Task2
+//
+//  Created by Kirill Netavskiy on 09.11.16.
+//  Copyright © 2016 Kirill Netavskiy. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
